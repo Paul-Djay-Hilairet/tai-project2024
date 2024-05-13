d85584a73ee31244642cbc330ff18dccfb35ae81
sylvainmzl <sylvain.mazel@etu.estia.fr>
2024-05-13 12:11:39 +0200

    DBUpdate 1

--- DBGesticom/tai.sql
+++ DBGesticom/tai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 13 mai 2024 à 09:11
+-- Généré le : lun. 13 mai 2024 à 10:11
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -40,7 +40,15 @@ CREATE TABLE IF NOT EXISTS `commande` (
   KEY `id_fournisseur` (`id_fournisseur`),
   KEY `id_product` (`id_product`),
   KEY `id_user` (`id_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id`, `id_fournisseur`, `id_product`, `id_user`, `reference`, `Date_commande`, `Etat_livraison`) VALUES
+(1, 2, 1, 1, 'OP10A46', '13/05/24', 'En cours'),
+(2, 5, 2, 1, 'VICSD48A25', '13/05/24', 'En cours');
 
 -- --------------------------------------------------------
 
@@ -77,7 +85,18 @@ CREATE TABLE IF NOT EXISTS `fournisseur` (
   `contact` varchar(100) NOT NULL,
   `Adresse` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `fournisseur`
+--
+
+INSERT INTO `fournisseur` (`id`, `name`, `contact`, `Adresse`) VALUES
+(1, 'Laguiole', 'contact@laguiole.com', '23 allée Amicale 12210 Laguiole '),
+(2, 'Opinel', 'contact@opinel.com', '508 Bd Henry Bordeaux, 73000 Chambéry'),
+(3, 'Fissler', 'contact@fissler.com', 'Harald-Fissler-Straße 10, 55768 Hoppstädten-Weiersbach, Allemagne'),
+(4, 'Couteaux Kai', 'contact@kai-europe.com', 'Kottendorfer Straße 5\r\n42697 Solingen\r\nAllemagne'),
+(5, 'Victorinox', 'contact@victorinox.com', 'Rte de Bâle 63, 2800 Delémont, Suisse');
 
 -- --------------------------------------------------------
 
@@ -98,7 +117,16 @@ CREATE TABLE IF NOT EXISTS `product` (
   `Poids` int(11) NOT NULL,
   `Origine` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `fournisseur`, `prix`, `Longueur_lame`, `Materiau_lame`, `Type_lame`, `Manche`, `Poids`, `Origine`) VALUES
+(1, 'N°10 Opinel Tradition', 'Opinel', 15, 10, 'Acier inoxydable ', 'Lame Yatagan', 'Bois hetre', 15, 'Savoie'),
+(2, 'Classic SD', 'Victorinox', 25, 5, 'Aluminium', 'Couteau-suisse', 'Plastique ABS', 25, 'Suisse'),
+(3, 'Couteau Santoku PROFI', 'Fissler', 30, 13, 'Acier inoxydable', 'Alvéolé', 'Plastique ABS', 160, 'Allemande');
 
 -- --------------------------------------------------------
 
@@ -109,11 +137,10 @@ CREATE TABLE IF NOT EXISTS `product` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `statut` int(5) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COMMENT='Example of user table for login info';
@@ -122,11 +149,11 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `firstname`, `lastname`, `login`, `password`, `creation_date`) VALUES
-(1, 'Homer', 'Simpson', 'donut', '202cb962ac59075b964b07152d234b70', '2024-01-03 15:41:56'),
-(2, 'Marge', 'Simpson', 'marge', '519b2f2d0d0c048c6a5d085f79d6012c', '2024-01-03 15:41:56'),
-(3, 'Bart', 'Simpson', 'el barto', '172924aadec293666b805437b84c18d7', '2024-01-03 15:41:56'),
-(4, 'Lisa', 'Simpson', 'lisa_simpson', 'f61f2e52cef0031f01f332033298f9e9', '2024-01-03 15:41:56');
+INSERT INTO `user` (`id`, `name`, `login`, `password`, `statut`) VALUES
+(1, 'Admin', 'admin.Gesticom', '202cb962ac59075b964b07152d234b70', 1),
+(2, 'Controleur', 'ctrl.Gesticom', '519b2f2d0d0c048c6a5d085f79d6012c', 2),
+(3, 'Commercial', 'commerce.Gesticom', '172924aadec293666b805437b84c18d7', 3),
+(4, 'Commercial2', 'commercial2.Gesticom', 'f61f2e52cef0031f01f332033298f9e9', 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

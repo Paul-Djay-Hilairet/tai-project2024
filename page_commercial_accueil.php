c14bee1cc68b3e02538f928f7b1231e5d1aae438
Paul-Djay-Hilairet <paul.dbjay-hilairet@etu.estia.fr>
2024-05-13 09:19:45 +0200

    template site

---
+++ page_commercial_accueil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Commandes en cours</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="commercial_accueil.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Commandes en cours</h1>
+        <div class="commands">
+            <div class="command">
+                <p>Référence commande: XXXFFP6354CD</p>
+                <p>Fournisseur: Mokuzai</p>
+                <p>Date de commande: 16/04/2024</p>
+                <p>État livraison commande: <span class="status received">Reçue</span></p>
+                <button class="details">Fiche technique détaillée</button>
+            </div>
+            <div class="command">
+                <p>Référence commande: XXXFFP9845CD</p>
+                <p>Fournisseur: Kiwadushi</p>
+                <p>Date de commande: 01/04/2024</p>
+                <p>État livraison commande: <span class="status delayed">En retard</span></p>
+                <button class="details">Fiche technique détaillée</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

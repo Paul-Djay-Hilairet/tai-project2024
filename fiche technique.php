a989f79897ab01a316ad5f23b646d1c67b01f462
Paul-Djay-Hilairet <paul.dbjay-hilairet@etu.estia.fr>
2024-05-13 11:51:18 +0200

    fiche technique page

---
+++ fiche technique.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fiche detaillé conformité</title>
+</head>
+<body>
+
+<p>Fiche conformité détaillée</p>
+<p>Nom du controleur: <a href="nomducontroleur"> mizobi </a></p>
+<p>Info commande : </p> <p> Numero de commande: <a href="numerocommande">165</a></p>
+
+<p>Nom du fournisseur : <a href="nomdufournisseur"> mizobi </a> </p>
+<?php
+echo "Date : " . date("d/m/Y");
+?>
+<p>Nombre de critère validés : <a href="NombredecritèreOK"> 15/25</a> </p>
+
+
+</body>
+</html>
